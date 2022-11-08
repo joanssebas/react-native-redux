ad3f5076b7aa4de6f9aadc6a81b06098d271ba84
Joanssebas <yoandec@hotmail.com>
2022-11-08 07:37:49 -0500

    actualizacion en el uso de redux

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, Button} from 'react-native';
 import {useAppSelector} from '../app/hooks';
+import {useSelector, useDispatch} from 'react-redux';
+import {decrement, increment} from '../features/counter/counterSlice';
 
 const Counter = () => {
-  const counter = useAppSelector(state => state.counter.value);
+  const {counter} = useSelector((state: any) => state.counter);
+  const dispatch = useDispatch();
+  // useAppSelector(state => state.counter.value);
 
   return (
     <View>
-      <Text>{counter} </Text>
+      <Text>el counter es {counter} </Text>
+
+      <TouchableOpacity
+        onPress={
+          () =>
+            // dispatch(increment()
+
+            dispatch(increment())
+
+          // )
+        }>
+        <Text>Increment</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => dispatch(decrement())}>
+        <Text>Decrement </Text>
+      </TouchableOpacity>
     </View>
   );
 };
